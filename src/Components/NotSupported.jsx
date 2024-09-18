da02daf106ab93190dacd9dae61d0b71babe2bdd
Kamal <bishnoikamal5529@gmail.com>
2024-09-18 02:01:29 -0400

    Not Supported Page Updated

--- src/Components/NotSupported.jsx
+++ src/Components/NotSupported.jsx
@@ -1,7 +1,16 @@
 
 function NotSupported(){
 
-    return <h1>Your Browser Does Not Support Speech Recognition</h1>
+    return <div className=" min-vh-100 min-vw-100 d-flex justify-content-center align-items-center flex-column">
+            <section className="h1"><span className="text-danger">Live</span>SpeechToText </section>
+
+            <button className="btn btn-close-white shadow-lg pe-5 rounded-4 ps-4">
+                Sorry, Your Browser Does Not Support Speech Recognition, Try Different Browser, Thanks
+            </button>
+            <p className=' container bg-body-secondary text-body-style m-5 rounded-4 text-dark-emphasis py-3 px-5 h5'>
+            
+            </p>
+        </div>
   
 }
 
